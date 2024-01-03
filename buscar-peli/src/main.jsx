f1957336fd5bffd270e810da9b23bc57658e044e
Imbol <mari_imbol@hotmail.com>
2024-01-03 11:35:21 -0500

    buscador de peliculas

---
+++ buscar-peli/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BuscarPeli } from './buscarPeli';
+import './buscarPeli.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuscarPeli />
+  </React.StrictMode>,
+)
